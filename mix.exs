906e7eabe8583121e9c97afe135156e28469162c
Naumenko Nikita <naumenkoniki@gmail.com>
2019-10-26 13:51:37 +0300

    add license

--- mix.exs
+++ mix.exs
@@ -3,12 +3,17 @@ defmodule BrainGames.MixProject do
 
   def project do
     [
+      name: "Brain Games",
       app: :brain_games,
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
-      escript: escript()
+      escript: escript(),
+      source_url: "https://github.com/NikitaNaumenko/brain_games_elixir",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -28,10 +33,24 @@ defmodule BrainGames.MixProject do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:ex_prompt, "~> 0.1.5"}
+    ]
+  end
 
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp description do
+    "Brain games is a simple cli utilite, wich made just for fun."
+  end
+
+  defp package do
+    [
+      maintainers: ["Naumenko Nikita"],
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/NikitaNaumenko/brain_games_elixir"}
     ]
   end
 end
